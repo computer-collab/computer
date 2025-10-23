b89f34b7ba374c7f956f3127786095d1fa041c11
computer-collab <computerforgithub@hotmail.com>
2025-10-23 07:17:25 +0530

    Finalization of the question 15

--- question15.c
+++ question15.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int notes_count();
+int notes_count(int amount){
+  int notes[6]={1,2,5,10,50,100};
+  int total_notes=0;
+  int denotion[6];
+  for (int i=5;i>=0;i--){
+
+      
+        if (amount >= notes[i]){
+          //printf("\n\ntotal notes %d",total_notes);
+          denotion[i]= (amount)/notes[i];
+          //printf("\ninitial amount = %d",amount);
+          amount %=notes[i];
+          //printf("\noperation by %d",notes[i]);
+          //printf("\namount %d",amount);
+          total_notes += denotion[i];
+          printf("\nThe number of notes of %d is   %d",notes[i],denotion[i]);
+          }else continue;
+          
+  }printf("\nThe total number of notes is %d",total_notes);
+  return 0;
+}
 
 
 int main() {
@@ -10,31 +30,9 @@ int main() {
   int denotion;
   printf("Please enter the amount You want to withdraw:  ");
   scanf("%d",&amount);
- // denotion=notes_count(amount);
-  //printf("%d",denotion);
+  notes_count(amount);
   
   printf("\nHello, World! End of the program notes counter!! ");
   return 0;
 }
 
-int notes_count(int amount){
-  int notes[6]={1,2,5,10,50,100};
-  int total_notes;
-  int denotion[6];
-  for (int i=5;i>=0;i--){
-    
-      if (amount>0){
-        if (amount >= notes[i]){
-          //printf("\n\ntotal notes %d",total_notes);
-          denotion[i]= (amount)/notes[i];
-          //printf("\ninitial amount = %d",amount);
-          amount %=notes[i];
-          //printf("\noperation by %d",notes[i]);
-          //printf("\namount %d",amount);
-          total_notes += denotion[i];
-          //printf("\nnumber  %d",notes[i],denotion[i]);
-          }else continue;
-          }
-  }
-  return total_notes;
-}
